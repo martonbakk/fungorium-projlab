b24e753b9a590fb28355a1e753f274fcf345bc07
Májer József Barnabás <mbarnikas@gmail.com>
2025-03-23 17:31:32 +0100

    hyphal aging halfdone

--- src/Hyphal.java
+++ src/Hyphal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Hyphal extends Entity {
 
     public Hyphal() {
@@ -7,6 +9,7 @@ public class Hyphal extends Entity {
 
     public void growFungus() {
         System.out.println("Hyphal is growing fungus");
+
     }
 
     public void speedUpDevelopment() {
@@ -14,7 +17,42 @@ public class Hyphal extends Entity {
     }
 
     public void aging() {
-        System.out.println("Hyphal is aging");
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("->update");
+
+        System.out.println("\"Are there any connected fungalbodies? Y/N\"");    
+
+        String answer = scanner.nextLine();
+        boolean check=true;
+
+        if (answer.equals("Y")){
+            System.out.println("Hyphal does not age.");    
+
+        }
+        while (check) {}
+            
+        
+        if (answer.equals("N")){
+            System.out.println("->aging");    
+
+            System.out.println("\"Does the hyphal have any more lives? Y/N\"");    
+            answer = scanner.nextLine();
+        
+    
+
+        if (answer.equals("Y")){
+            System.out.println("Repeats the first part.");    
+
+        }
+
+        if (answer.equals("N")){
+            System.out.println("Hphal dies");    
+        }
+
+        }
+    
     }
 
     public void update() {
