d1caaaaf5a2c692508093a696755fb324c43b455
Bakk Márton <marci.bakk2004@gmail.com>
2025-03-23 16:53:06 +0100

    Hyphal functions printing now

--- src/Hyphal.java
+++ src/Hyphal.java
@@ -6,18 +6,18 @@ public class Hyphal extends Entity {
     }
 
     public void growFungus() {
-
+        System.out.println("Hyphal is growing fungus");
     }
 
     public void speedUpDevelopment() {
-
+        System.out.println("Hyphal is speeding up development");
     }
 
     public void aging() {
-        
+        System.out.println("Hyphal is aging");
     }
 
     public void update() {
-        
+        System.out.println("Hyphal is updated");
     }
 }
