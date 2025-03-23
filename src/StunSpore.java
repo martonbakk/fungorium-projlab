c6b891cd6584adedef7f4ade21ee349a32dc3f7b
Májer József Barnabás <mbarnikas@gmail.com>
2025-03-23 16:39:57 +0100

    stunspore is done

--- src/StunSpore.java
+++ src/StunSpore.java
@@ -5,4 +5,16 @@ public class StunSpore extends Spore{
         System.out.println("new StunSpore Created");
     }
 
+    public void activateEffect(){
+        System.out.println("Stun effect activated");
+    }
+    
+    public void spawnSpore(){
+        System.out.println("StunSpore is spawned");
+    }
+
+    public void update() {
+        System.out.println("StunSpore is updated");
+    }
+
 }
