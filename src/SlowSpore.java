4d5a9b373eb00615165e3f82e26c5f07f3173467
Májer József Barnabás <mbarnikas@gmail.com>
2025-03-23 16:39:18 +0100

    slowspore is done

--- src/SlowSpore.java
+++ src/SlowSpore.java
@@ -5,4 +5,16 @@ public class SlowSpore extends Spore{
         System.out.println("new SlowSpore Created");
     }
 
+    public void activateEffect(){
+        System.out.println("Slow effect activated");
+    }
+    
+    public void spawnSpore(){
+        System.out.println("SlowSpore is spawned");
+    }
+
+    public void update() {
+        System.out.println("SlowSpore is updated");
+    }
+
 }
