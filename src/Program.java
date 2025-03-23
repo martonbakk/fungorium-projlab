02b15e40ffcf6a96ceaf725afd6104f00b3d900d
Bakk Márton <marci.bakk2004@gmail.com>
2025-03-23 15:53:52 +0100

    Added skeleton for function print tests

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,15 @@
 public class Program {
     public static void main(String[] args) {
+        Map map = new Map();
+        map.generateMap();
+        map.splitTecton();
+        map.updateTectons();
+
+        Insect insect = new Insect();
+        insect.move();
+        insect.eatSpore();
+        insect.cutHyphal();
+        insect.update();
+
     }
 }
