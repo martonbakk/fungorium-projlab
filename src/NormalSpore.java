7b8944fa6ae5adfb540136b4035530989b4846fa
Májer József Barnabás <mbarnikas@gmail.com>
2025-03-23 16:41:13 +0100

    normalspore is done

--- src/NormalSpore.java
+++ src/NormalSpore.java
@@ -3,4 +3,16 @@ public class NormalSpore extends Spore {
         super();
         System.out.println("new NormalSpore Created");
     }
+
+    public void activateEffect(){
+        System.out.println("Normal effect activated");
+    }
+    
+    public void spawnSpore(){
+        System.out.println("NormalSpore is spawned");
+    }
+
+    public void update() {
+        System.out.println("NormalSpore is updated");
+    }
 }
