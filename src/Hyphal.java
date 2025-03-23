bac29e643b4f4e39c14f55addad7965c53b7339e
Májer József Barnabás <mbarnikas@gmail.com>
2025-03-23 17:59:26 +0100

    speedupdevelopment done

--- src/Hyphal.java
+++ src/Hyphal.java
@@ -13,7 +13,19 @@ public class Hyphal extends Entity {
     }
 
     public void speedUpDevelopment() {
-        System.out.println("Hyphal is speeding up development");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\"Are there enough spores on the target tecton? Y/N\""); 
+
+        String answer = scanner.nextLine();
+
+        if (answer.equals("Y")){
+
+            System.out.println("->decreaseSpores()");
+            System.out.println("Spores destroyed");
+            System.out.println("Hyphal is speeding up development");
+        }
+
     }
 
     public void aging() {
@@ -30,22 +42,13 @@ public class Hyphal extends Entity {
         if (answer.equals("Y")){
             System.out.println("Hyphal does not age.");    
 
-        }
-        while (check) {}
-            
+        }            
         
         if (answer.equals("N")){
             System.out.println("->aging");    
 
             System.out.println("\"Does the hyphal have any more lives? Y/N\"");    
             answer = scanner.nextLine();
-        
-    
-
-        if (answer.equals("Y")){
-            System.out.println("Repeats the first part.");    
-
-        }
 
         if (answer.equals("N")){
             System.out.println("Hphal dies");    
