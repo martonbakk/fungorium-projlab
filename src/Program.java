955e28ae5009dd5da005877cfdbfa63e2a2adf65
Ilyés Gergely <gergelyilyes@gmail.com>
2025-03-24 16:53:58 +0100

    Program changes

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,13 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Program {
+
+    private static HashMap<String, Object> objectsMap = new HashMap<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        initialize();
         while (true) {
             printTestCases();
             
@@ -31,11 +35,16 @@ public class Program {
                     fungalBodyShootsSpore();
                     break;
                 case "7":
+                    growHyphal();
                     break;
                 case "8":
-                    keepHyphalAlive();
+                    growFungalBody();
                     break;
                 case "9":
+                keepHyphalAlive();
+                    break;
+                case "10":
+                    hyphalAging();
                     break;
                 default:
                     System.out.println("Nincs ilyen teszt.");
@@ -44,46 +53,90 @@ public class Program {
         }
     }
 
+    private static void initialize() {
+        Tecton t1 = new Tecton();
+        ToxicTecton tt1 = new ToxicTecton();
+        WeakTecton wt1 = new WeakTecton();
+        NoFungusTecton nft1 = new NoFungusTecton();
+
+        NormalSpore nsp1 = new NormalSpore();
+        SlowSpore slowsp1 = new SlowSpore();
+        SpeedSpore speedsp1 = new SpeedSpore();
+        StunSpore stunsp1 = new StunSpore();
+        HyphalProtectorSpore hpsp1 = new HyphalProtectorSpore();
+        HungerSpore hsp1 = new HungerSpore();
+
+        Insect i1 = new Insect();
+        Hyphal h1 = new Hyphal();
+        FungalBody fgb1 = new FungalBody();
+
+        objectsMap.put("Tecton", t1);
+        objectsMap.put("ToxicTecton", tt1);
+        objectsMap.put("WeakTecton", wt1);
+        objectsMap.put("NoFungusTecton", nft1);
+        objectsMap.put("NormalSpore", nsp1);
+        objectsMap.put("SlowSpore", slowsp1);
+        objectsMap.put("SpeedSpore", speedsp1);
+        objectsMap.put("StunSpore", stunsp1);
+        objectsMap.put("HyphalProtectorSpore", hpsp1);
+        objectsMap.put("HungerSpore", hsp1);
+        objectsMap.put("Insect", i1);
+        objectsMap.put("Hyphal", h1);
+        objectsMap.put("FungalBody", fgb1);
+    }
+
     private static void splitTecton(){
         Map map = new Map();
         map.splitTecton();
     }
 
     private static void insectEatsSpore(){
-        Insect insect = new Insect();
+        Insect insect = (Insect)objectsMap.get("Insect");
         insect.eatSpore();
     }
     
     private static void moveInsect() {
-        Insect insect = new Insect();
+        Insect insect = (Insect)objectsMap.get("Insect");
         insect.move();
     }
     
     private static void insectCutHyphal(){
-        Insect insect = new Insect();
+        Insect insect = (Insect)objectsMap.get("Insect");
         insect.cutHyphal();
     }
 
     private static void speedUpHyphalDevelopment(){
-        Hyphal hyphal = new Hyphal();
+        Hyphal hyphal = (Hyphal)objectsMap.get("Hyphal");
         hyphal.speedUpDevelopment();
     }
 
     private static void fungalBodyShootsSpore(){
-        FungalBody fungus = new FungalBody();
+        FungalBody fungus = (FungalBody)objectsMap.get("FungalBody");
         fungus.shootSpore();
     }
 
     private static void keepHyphalAlive() {
-        FungalBody fungus = new FungalBody();
+        FungalBody fungus = (FungalBody)objectsMap.get("FungalBody");
         fungus.keepHyphalAlive();
     }
 
     // TODO
-    /*private static void growHyphal(){
-        FungalBody fungus = new FungalBody();
-        fungus.growHyphal();
-    }*/
+    private static void growHyphal() {
+        FungalBody fungus = (FungalBody)objectsMap.get("FungalBody");
+        //fungus.growHyphal();
+    }
+
+    // TODO
+    private static void growFungalBody() {
+        FungalBody fungus = (FungalBody)objectsMap.get("FungalBody");
+        //fungus.growFungalBody();
+    }
+
+    // TODO
+    private static void hyphalAging() {
+        Hyphal hyphal = (Hyphal)objectsMap.get("Hyphal");
+        //hyphal.aging()
+    }
 
     private static void printTestCases() {
         System.out.println("Choose Test");
@@ -95,9 +148,9 @@ public class Program {
         System.out.println("5. Split Tecton");
         System.out.println("6. FungalBody Shoots Spore");
         System.out.println("7. Grow Hyphal");
-        System.out.println("7. Grow FungalBody");
-        System.out.println("8. Keep Connected Hyphals Alive");
-        System.out.println("9. Hyphal dies due to aging");
+        System.out.println("8. Grow FungalBody");
+        System.out.println("9. Keep Connected Hyphals Alive");
+        System.out.println("10. Hyphal dies due to aging");
     }
 
 }
