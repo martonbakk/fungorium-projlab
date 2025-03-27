57334c1830e1618d51d3e6b3d6603d96f6b7522a
Ilyés Gergely <gergelyilyes@gmail.com>
2025-03-27 22:40:50 +0100

    Test Files added

---
+++ src/test/java/hu/bme/iit/projlab/bmekings/Map/MapTest.java
@@ -0,0 +1,18 @@
+package hu.bme.iit.projlab.bmekings.Map;
+
+import org.junit.jupiter.api.Test;
+
+class MapTest {
+
+    @Test
+    void generateMap() {
+    }
+
+    @Test
+    void splitTecton() {
+    }
+
+    @Test
+    void updateTectons() {
+    }
+}
